bed7582815da3e43b0b3a96ddbab8dbcd1897727
SukainaR <rizwansukaina@gmail.com>
2024-01-03 16:52:11 +0000

    Update Landmark.cs

--- Assets/Scripts/API/Landmark.cs
+++ Assets/Scripts/API/Landmark.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Landmark : MonoBehaviour
+[SerializeField]
+public class Landmark 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int land_id { get; set; }
+    public string land_name { get; set; }
+    public string land_head_1 { get; set; }
+    public string land_text_1 { get; set; }
+    public string land_head_2 { get; set; }
+    public string land_text_2 { get; set; }
+    public string land_head_3 { get; set; }
+    public string land_text_3 { get; set; }
+    public string land_head_4 { get; set; }
+    public string land_text_4 { get; set; }
+    public string land_img_1 { get; set; }
+    public string land_img_2 { get; set; }
+    public string land_img_3 { get; set; }
+    public int land_map_id { get; set; }
 }
