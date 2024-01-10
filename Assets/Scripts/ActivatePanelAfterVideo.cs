509de97cd46f0629431af8b776a651b0646607dd
SukainaR <rizwansukaina@gmail.com>
2024-01-10 01:17:12 +0000

    video files to github

---
+++ Assets/Scripts/ActivatePanelAfterVideo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class ActivatePanelAfterVideo : MonoBehaviour
+{
+    public VideoPlayer videoPlayer; // Drag & Drop the GameObject holding the VideoPlayer component
+    public GameObject Panel;
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += LoadPanel;
+    }
+
+    public void LoadPanel(VideoPlayer vp)
+    {
+        Panel.gameObject.SetActive(true);
+
+    }
+}
